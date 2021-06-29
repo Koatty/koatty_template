47057b4a1ec8816f2651a3bc6b30b3fd272a1091
linyyyang <linyyyang@tencent.com>
2021-06-29 17:21:19 +0800

    🐞 fix: 更新依赖

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,12 +3,12 @@
  * @Usage: 静态配置数据信息
  * @Author: xxx
  * @Date: 2020-12-22 15:24:25
- * @LastEditTime: 2021-06-24 16:39:22
+ * @LastEditTime: 2021-06-24 17:08:48
  */
 export default {
     /*app config*/
     app_port: 3000, // 监听端口
-    app_hostname: '', // Hostname
+    app_host: '', // Hostname
     encoding: 'utf-8', //输出数据的编码
 
     // logs_level: "INFO", // Level log is printed to the console, "DEBUG" | "INFO" | "WARN" | "ERROR"
