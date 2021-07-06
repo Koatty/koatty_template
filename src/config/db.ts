df4ef73c78bd2db4675ec9b98a2ddb7fceda8fc4
linyyyang <linyyyang@tencent.com>
2021-07-06 17:04:27 +0800

    🐞 fix: add CacheStore

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@
  * @Usage: 包括database、redis配置信息
  * @Author: xxx
  * @Date: 2020-12-22 15:24:25
- * @LastEditTime: 2021-07-01 15:40:21
+ * @LastEditTime: 2021-07-06 17:04:05
  */
 
 export default {
@@ -18,5 +18,19 @@ export default {
     //     db_prefix: '',
     //     db_charset: 'utf8'
     // },
+
+    "CacheStore": {
+        type: "memory", // redis or memory
+        // key_prefix: "koatty",
+        // host: '127.0.0.1',
+        // port: 6379,
+        // name: "",
+        // username: "",
+        // password: "",
+        // db: 0,
+        // timeout: 30,
+        // pool_size: 10,
+        // conn_timeout: 30
+    },
 
 };
