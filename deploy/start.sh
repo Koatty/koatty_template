5a630019017572e778d6a7561635c0858f1cd294
linyyyang <linyyyang@tencent.com>
2022-02-25 15:02:16 +0800

    ci: 增加deploy配置

---
+++ deploy/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "install nodejs modules ----------------------------------"
+yarn install
+
+echo "start pm2 -----------------------------------------------"
+pm2 startOrGracefulReload /pm2.json
+
+echo "running -------------------------------------------------"
+pm2 log
