5cbe4b85001646f1f12d9361ae840676155b8f95
linyyyang <linyyyang@tencent.com>
2022-05-27 11:56:52 +0800

    build: update

--- jest.config.js
+++ jest.config.js
@@ -25,12 +25,10 @@ module.exports = {
   // 将 `ts-jest` 的配置注入到运行时的全局变量中
   globals: {
     'ts-jest': {
-      // 是否使用 babel 配置来转译
-      babelConfig: true,
       // 编译 Typescript 所依赖的配置
-      tsconfig: '<rootDir>/tsconfig.test.json',
+      tsconfig: '<rootDir>/tsconfig.json',
       // 是否启用报告诊断，这里是不启用
       diagnostics: false,
     },
-  },
+  }
 };
