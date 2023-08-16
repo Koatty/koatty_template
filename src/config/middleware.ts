ba9ead5d9694f74b71edf79d26f2ee82c1a6055d
linyyyang <linyyyang@tencent.com>
2023-08-16 17:09:06 +0800

    fix: apidoc

--- src/config/middleware.ts
+++ src/config/middleware.ts
@@ -3,18 +3,16 @@
  * @usage: 配置待加载的中间件及加载顺序
  * @Author: xxx
  * @Date: 2020-12-22 15:24:25
- * @LastEditTime: 2023-02-19 01:32:34
+ * @LastEditTime: 2023-08-16 17:05:48
  */
 
 export default {
   list: ["StaticMiddleware", "ViewMiddleware"], //加载的中间件列表
   config: { //中间件配置
-    // 静态服务器中间件默认未开启
-    StaticMiddleware: false,
-    // 需要开启请修改为:
-    // StaticMiddleware: {
-    //     cache: true
-    // },
+    // 静态服务器中间件
+    StaticMiddleware: {
+      cache: true
+    },
     ViewMiddleware: {
       root: `${process.env.ROOT_PATH}/view`, // 模板目录
       opts: {
