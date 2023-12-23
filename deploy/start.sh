027e542b69a1ce7b604ec7b517f863d04d448af6
linyyyang <linyyyang@tencent.com>
2023-12-23 11:48:43 +0800

    chore: dockerfile

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 echo "install nodejs modules ----------------------------------"
-cd /data && yarn install -s
+cd /data && 
+
+if [ -f "/data/pnpm-lock.lock" ]; then
+  # used pnpm
+  pnpm install --production
+elif [ -f "/data/yarn.lock" ]; then
+  # used yarn  
+  yarn install --production
+else
+  # used npm
+  npm install --production
+fi
 
 echo "start pm2 -----------------------------------------------"
 pm2 startOrGracefulReload /pm2.json
--- src/middleware/StaticMiddleware.ts
+++ src/middleware/StaticMiddleware.ts
@@ -3,16 +3,16 @@
  * @Usage: 提供静态文件服务
  * @Author: xxx
  * @Date: 2020-12-22 15:24:25
- * @LastEditTime: 2023-11-11 11:40:26
+ * @LastEditTime: 2023-12-23 11:33:51
  */
 
 import { Middleware, IMiddleware } from 'koatty';
 import { App } from '../App';
-import { Static } from "koatty_static";
+import { KoattyStatic } from "koatty_static";
 
 @Middleware()
 export class StaticMiddleware implements IMiddleware {
   run(options: any, app: App) {
-    return Static(options, app);
+    return KoattyStatic(options, app);
   }
 }
