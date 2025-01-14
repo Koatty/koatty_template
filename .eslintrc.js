92cd1ed79f5da42c6c5f5afdff439275c2b01fc4
richenlin <richenlin@gmail.com>
2025-01-14 16:33:01 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-unsafe-function-type": "warn",
     '@typescript-eslint/no-unused-vars': "warn"
   },
 };
